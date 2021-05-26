bdee122e061c5af3f35f0c1c0586bc2927a7684a
greigja <jagreig@computer.org>
2021-05-26 12:34:54 -0400

    housekeeping - comments

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { ProjectInput } from './components/project-input.js';
 import { ProjectList } from './components/project-list.js';
 
+// Instantiations build web app via constructors
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
