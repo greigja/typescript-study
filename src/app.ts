6e316f08574f390bbceafe1b95aad2f99e773783
greigja <jagreig@computer.org>
2021-05-27 21:20:43 -0400

    clean up

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectInput } from './components/project-input.js';
-import { ProjectList } from './components/project-list.js';
-
-// Instantiations build web app via constructors
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
